ca97d97a998c79d8cc807b2d238aec0617a67d45
annlita <annlitajoshy@gmail.com>
2025-11-29 02:36:44 -0500

    website done

---
+++ server.js
@@ -0,0 +1,42 @@
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+function loadUsers() {
+    const filePath = path.join(__dirname, "users.json");
+    const data = fs.readFileSync(filePath, "utf8");
+    return JSON.parse(data);
+}
+
+const users = loadUsers();
+
+app.post("/login", (req, res) => {
+    const { username, password } = req.body;
+
+    const validUser = users.find(
+        (u) => u.username === username && u.password === password
+    );
+
+    if (validUser) {
+        return res.redirect("/success.html");
+    }
+
+    // send back the index page WITH an error message
+    const loginPage = fs.readFileSync("./public/index.html", "utf8");
+    const errorInjected = loginPage.replace(
+        "<div class=\"login-box\">",
+        `<div class="login-box">
+            <p class="error">❌ Incorrect username or password</p>`
+    );
+
+    res.send(errorInjected);
+});
+
+app.listen(3000, () => {
+    console.log("Server running on http://localhost:3000");
+});
